6323a871e08c68cc69f1dd544210dd5a8d0da39a
Ritoban Goswami <ritobangoswami15@gmal.com>
2022-08-26 01:22:40 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const hr = document.getElementById("hr");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours();
+    let mm = day.getMinutes();
+    let ss = day.getSeconds();
+    let ms = day.getMilliseconds();
+    ss = ss + ms / 1000;
+    mm = mm + (ss / 60);
+
+    hr.style.transform = `rotateZ(${((hh) * 30) + (mm) / 12}deg)`;
+    min.style.transform = `rotateZ(${((mm + 50.5) * 360 / 60)}deg)`;
+    sec.style.transform = `rotateZ(${((ss) + 23) * 6}deg)`;
+    console.log(hh, mm, ss, ms);
+}, 20);
